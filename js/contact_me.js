d6a35d7319f9ccb1c9e6a9ff849f9f9c82523dbe
Audrey McNay <amcnay@utexas.edu>
2018-04-21 14:37:22 -0500

    stinky

--- js/contact_me.js
+++ js/contact_me.js
@@ -8,6 +8,7 @@ $(function() {
     submitSuccess: function($form, event) {
       event.preventDefault(); // prevent default submit behaviour
       // get values from FORM
+      var orgname = $("input#orgname").val();
       var name = $("input#name").val();
       var email = $("input#email").val();
       var phone = $("input#phone").val();
